54589ec8ac9044a1421cf502d26644cc846ebebb
Pocieszny <48619590+Pocieszny@users.noreply.github.com>
2021-02-22 21:05:52 +0100

    Add files via upload

---
+++ abc.cpp
@@ -0,0 +1,32 @@
+/*
+    Project: abc bruteforce
+    Author: Pocieszny
+*/
+
+#include <iostream>
+
+int main ()
+{
+    unsigned char alph[3]       {'a', 'b', 'c'};
+    unsigned short int num[3]   {0, 0, 0};
+
+    while (true)
+    {
+        std::cout << alph[num[0]] << alph[num[1]] << alph[num[2]] << '\n';
+        
+        if (num[2] < 2)
+            ++num[2];
+        else if (num[1] < 2)
+        {
+            ++num[1];
+            num[2] = 0;
+        } else
+        {
+            if (num[0] == 2)
+                break;
+            ++num[0];
+            num[1] = 0;
+            num[2] = 0;
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,48 @@
+/*
+    [Project] : Brutforce Algorithm
+    [Author]  : Pocieszny
+*/ 
+
+#include <iostream>
+
+constexpr short int len     {10};
+constexpr short int alen    {6};
+
+unsigned char alph[alen + 1]    {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+unsigned short int num[len + 1] {};
+
+bool isDone ();
+
+inline void output ()
+{
+    for (unsigned short int x {0}; x <= len; ++x)
+        std::cout << alph[num[x]];
+    std::cout << '\n';
+}
+
+int main ()
+{
+    while (isDone())
+    {
+        output();
+
+        for (short int c {len}; c >= 0; --c)
+        {
+            if (num[c] < alen)
+            {
+                ++num[c];
+                break;
+            } else
+                num[c] = 0;
+        }
+    }
+}
+
+bool isDone ()
+{
+    for (unsigned short int x {0}; x <= len; ++x)
+        if (num[x] != alen)
+            return true;
+    output();
+    return false;
+}
